f0ebb4bedc0862f5f79f64367e657e70cae54305
khush <khushramnani@gmail.com>
2025-05-30 15:18:45 +0530

    feat: add ESLint configuration and Vercel build environment settings

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-function-type': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+  },
+};
